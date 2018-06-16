492cd9bde318571b46bddf690f49b8c57cdd3499
Yaroslav Volohov <yvolohov@gmail.com>
2018-06-16 13:41:38 +0300

    initial commit

---
+++ task-1/index.js
@@ -0,0 +1,15 @@
+/**
+ * Создает матрицу размером n * n и заполняет ее по спирали
+ *
+ * @param {Number} n - размерность матрицы
+ * @returns {Number[n][n]} - n * n - матрица, заполненная по спирали
+ */
+function fillSpiralMatrix(n) {
+  const result = [[]];
+
+  // Ваш код
+
+  return result;
+}
+
+export default fillSpiralMatrix;
