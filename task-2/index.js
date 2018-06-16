a1ff9f5d6b4ecaf30b66aee3a720021b41d5a950
Yaroslav Volohov <yvolohov@gmail.com>
2018-06-16 14:32:08 +0300

    task-2

--- task-2/index.js
+++ task-2/index.js
@@ -8,6 +8,21 @@
  * @returns {Boolean}
  */
 function haveSameItems(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let copyArr2 = arr2.slice();
+  copyArr2.sort();
+
+  for (let idx = 0; idx < arr1.length; idx++) {
+    let el1 = arr1[idx];
+    let el2 = copyArr2[idx];
+
+    if (el1 !== el2) {
+      return false;
+    }
+  }
   return true;
 }
 
